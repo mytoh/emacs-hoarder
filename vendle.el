7ddb23fb7aa6a7208f00ee3de1eaf531245f61c0
mytoh <mytoh@gmail.com>
2014-01-20 23:13:38 +0900

    vendle.el: update

--- vendle.el
+++ vendle.el
@@ -4,46 +4,11 @@
 (eval-when-compile
   (require 'cl-lib))
 
+(require 'vendle-source-github "vendle/source/github")
+(require 'vendle-source-git "vendle/source/git")
+(require 'vendle-package "vendle/package")
+
 ;;; internal functions
-
-(cl-defun vendle:source-git-p (source)
-  (cond ((or (string-match (rx "git://") source)
-             (string-match (rx ".git" (zero-or-one "/") line-end) source))
-         t)
-        (t nil)))
-
-(cl-defun vendle:source-github-p (source)
-  (cond ((string-match (rx "github:" (submatch (+ (or (syntax word) (syntax symbol)))
-                                               "/"
-                                               (+ (or (syntax word) (syntax symbol)))))
-                       source)
-         t)
-        ((string-match (rx   line-start
-                             (one-or-more (or (syntax symbol) (syntax word)))
-                             "/"
-                             (one-or-more (or (syntax symbol)
-                                              (syntax word)))
-                             line-end)
-                       source)
-         t)
-        (t nil)))
-
-(cl-defun vendle:source-format-github (source)
-  (cond
-   ((string-match (rx "github:" (submatch (+ (or (syntax word) (syntax symbol)))
-                                          "/"
-                                          (+ (or (syntax word) (syntax symbol)))))
-                  source)
-    (match-string-no-properties 1 source))
-   ((string-match (rx   line-start
-                        (one-or-more (or (syntax symbol) (syntax word)))
-                        "/"
-                        (one-or-more (or (syntax symbol)
-                                         (syntax word)))
-                        line-end)
-                  source)
-    (match-string-no-properties 0 source))))
-
 
 (cl-defun vendle:directory-git-p (p)
   (if (file-directory-p (expand-file-name ".git" p))
@@ -55,8 +20,8 @@
 (cl-defun vendle:add-to-theme-path (path)
   (add-to-list 'custom-theme-load-path path))
 
-(cl-defun vendle:add-to-package-list (path)
-  (add-to-list '*vendle-package-list* path))
+(cl-defun vendle:add-to-package-list (package)
+  (add-to-list '*vendle-package-list* package))
 
 ;;; utilily functions
 
@@ -65,19 +30,16 @@
 
 ;;;; initialize
 
-(defvar *vendle-directory* "")
+(defvar *vendle-directory* (expand-file-name (file-name-as-directory "vendle") user-emacs-directory))
 (defvar *vendle-package-list* '())
 
 (cl-defun vendle:initialize (&optional path)
   (setq *vendle-package-list* nil)
-  (if path
-      (setq *vendle-directory* path)
-    (setq *vendle-directory*
-          (expand-file-name (file-name-as-directory "vendle") user-emacs-directory)))
+  (when path
+    (setq *vendle-directory* path))
   (unless (file-exists-p *vendle-directory*)
     (make-directory *vendle-directory*))
   *vendle-directory*)
-
 
 ;;;; update
 
@@ -164,71 +126,6 @@
     (cl-mapc (lambda (p) (delete-directory p t))
              paths)))
 
-;;;; package
-
-(cl-defstruct vendle:package
-  type name url path)
-
-(cl-defun vendle:make-package (source info)
-  (cond ((vendle:source-github-p source)
-         (vendle:make-package-github
-          (vendle:source-format-github source) info))))
-
-(cl-defun vendle:make-package-github (source info)
-  (make-vendle:package :type 'github
-                       :name (vendle:make-package-name source info)
-                       :path (vendle:make-package-path source info)
-                       :url (cl-concatenate 'string "git://github.com/" source)))
-
-(cl-defun vendle:make-package-local (source info)
-  (make-vendle:package :type 'local
-                       :name (vendle:make-package-name-local source info)
-                       :path source
-                       :url (vendle:make-package-url-local source info)))
-
-(cl-defun vendle:make-package-name (source info)
-  (cond ((vendle:source-github-p source)
-         (vendle:make-package-name-github
-          (vendle:source-format-github source) info))))
-
-(cl-defun vendle:make-package-name-github (source info)
-  (if info
-      (cl-letf ((name (cl-getf info :name)))
-        (if name
-            name
-          (cadr (split-string source "/"))))
-    (cadr (split-string source "/"))))
-
-(cl-defun vendle:make-package-name-local (source info)
-  (if info
-      (cl-letf ((name (cl-getf info :name)))
-        (if name
-            name
-          (file-name-nondirectory source)))
-    (file-name-nondirectory source)))
-
-(cl-defun vendle:make-package-path (source info)
-  (cond ((vendle:source-github-p source)
-         (vendle:make-package-path-github (vendle:source-format-github source) info))))
-
-(cl-defun vendle:make-package-path-github (source info)
-  (cl-letf ((path (if info
-                      (cl-letf ((path (cl-getf info :path))
-                                (name (vendle:make-package-name source info)))
-                        (if path
-                            (cl-concatenate 'string
-                                            name  "/"  path)
-                          name))
-                    (vendle:make-package-name source info))))
-    (expand-file-name path *vendle-directory*)))
-
-(cl-defun vendle:make-package-url-local (source info)
-  (if info
-      (cl-letf ((url (cl-getf info :url)))
-        (if url
-            url
-          nil))
-    nil))
 
 ;; commands
 (cl-defun vendle-install ()
