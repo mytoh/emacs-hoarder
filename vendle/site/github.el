b19fdbcbb150a0a7db5a50af2dd4fc55176488f1
mytoh <mytoh@gmail.com>
2014-03-04 07:12:18 +0900

    * vendle/site/github.el: allow dot character in source

--- vendle/site/github.el
+++ vendle/site/github.el
@@ -1,33 +1,42 @@
 ;;; github.el -*- lexical-binding: t -*-
 
 (cl-defun vendle:source-site-github-p (source)
-  (cond ((string-match (rx "github:" (submatch (+ (or (syntax word) (syntax symbol)))
-                                               "/"
-                                               (+ (or (syntax word) (syntax symbol)))))
-                       source)
-         t)
-        ((string-match (rx   line-start
-                             (one-or-more (or (syntax symbol) (syntax word)))
-                             "/"
-                             (one-or-more (or (syntax symbol)
-                                              (syntax word)))
-                             line-end)
-                       source)
-         t)
-        (t nil)))
+  (cond
+   ((string-match (rx "github:" (submatch (+ (or (syntax word)
+                                                 (syntax symbol)
+                                                 (syntax punctuation)))
+                                          "/"
+                                          (+ (or (syntax word)
+                                                 (syntax symbol)
+                                                 (syntax punctuation)))))
+                  source)
+    t)
+   ((string-match (rx   line-start
+                        (one-or-more (or (syntax symbol)
+                                         (syntax word)
+                                         (syntax punctuation)))
+                        "/"
+                        (one-or-more (or (syntax symbol)
+                                         (syntax word)
+                                         (syntax punctuation)))
+                        line-end)
+                  source)
+    t)
+   (t nil)))
 
 (cl-defun vendle:source-site-format-github (source)
   (cond
-   ((string-match (rx "github:" (submatch (+ (or (syntax word) (syntax symbol)))
+   ((string-match (rx "github:" (submatch (+ (or (syntax word) (syntax symbol) (syntax punctuation)))
                                           "/"
-                                          (+ (or (syntax word) (syntax symbol)))))
+                                          (+ (or (syntax word) (syntax symbol) (syntax punctuation)))))
                   source)
     (match-string-no-properties 1 source))
    ((string-match (rx   line-start
-                        (one-or-more (or (syntax symbol) (syntax word)))
+                        (one-or-more (or (syntax symbol) (syntax word) (syntax punctuation)))
                         "/"
                         (one-or-more (or (syntax symbol)
-                                         (syntax word)))
+                                         (syntax word)
+                                         (syntax punctuation)))
                         line-end)
                   source)
     (match-string-no-properties 0 source))))
