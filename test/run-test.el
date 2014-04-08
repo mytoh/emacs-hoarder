416a9872336eb611c77959f6eee41701d94e16dd
mytoh <mytoh@gmail.com>
2014-04-08 12:17:26 +0900

    * test/run-test.el: add initialize test

--- test/run-test.el
+++ test/run-test.el
@@ -6,9 +6,13 @@
 (add-to-list 'load-path default-directory)
 (require 'vendle)
 
-(ert-deftest vendle-test-vendle-path ()
-  (should vendle-directory)
-  (should (string-equal vendle-directory (file-name-as-directory (expand-file-name "~/.emacs.d/vendle")))))
+(ert-deftest vendle-test-initialize ()
+  (vendle:initialize)
+  (should (string-equal vendle-directory (file-name-as-directory (expand-file-name "~/.emacs.d/vendle"))))
+  (should (file-exists-p vendle-directory))
+  (should (file-directory-p vendle-directory))
+  (when (file-exists-p vendle-directory)
+    (delete-directory vendle-directory 'recursive)))
 
 (cl-defun main ()
   (ert-run-tests-batch-and-exit))
