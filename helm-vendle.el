df76e4ba1c5cf762d4486f233514a2b17160697a
mytoh <mytoh@gmail.com>
2014-09-02 16:15:02 +0900

    * helm-vendle.el: add actions

--- helm-vendle.el
+++ helm-vendle.el
@@ -15,6 +15,24 @@
 (cl-defun helm-vendle-action-reinstall (candidate)
   (vendle:reinstall-package candidate))
 
+;; http://rubikitch.com/2014/09/02/helm-quelpa/
+(cl-defun helm-vendle-action-magit-log (package)
+  (cl-letf ((default-directory (vendle:package-path package)))
+    (magit-status ".")
+    (magit-log)))
+
+(cl-defun helm-vendle-action-open-dired (package)
+  (dired (vendle:package-path package)))
+
+(defun helm-vendle-action-find-file (package)
+  (helm-find-files-1 (vendle:package-path package)))
+
+(cl-defun helm-vendle-action-view-readme-or-src (package)
+  (cl-loop for file in (list "README.md" "README.org" "README")
+     for path = (expand-file-name file (vendle:package-path package))
+     when (file-exists-p path)
+     return (view-file path)))
+
 (cl-defun helm-vendle-source-name/mark (mark name)
   (cond ((window-system)
          (format " %s %s" mark name))
@@ -37,7 +55,11 @@
     (init . helm-vendle-init)
     (candidates . helm-vendle-candidates)
     (action . (("Update" . helm-vendle-action-update)
-               ("Reinstall" . helm-vendle-action-reinstall)))
+               ("Reinstall" . helm-vendle-action-reinstall)
+               ("Magit log" . helm-vendle-action-magit-log)
+               ("View README or source" . helm-vendle-action-view-readme-or-src)
+               ("Open directory" . helm-vendle-action-open-dired)
+               ("Find file" . helm-vendle-action-find-file)))
     (candidate-transformer
      helm-vendle-transformer-format)))
 
