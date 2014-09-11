477aa991dfd6ce9eb4ea0bf9b7b1a4ee9220e7ed
mytoh <mytoh@gmail.com>
2014-09-12 00:20:55 +0900

    * vendle/option/info.el: add single info directory support

--- vendle/option/info.el
+++ vendle/option/info.el
@@ -10,14 +10,19 @@
 (require 'vendle-package "vendle/package")
 
 (defmethod vendle:option-info ((package vendle:package))
-  (cl-typecase (vendle:package-info package)
-    (list
-     (cl-mapc
-      (lambda (path)
-        (setenv "INFOPATH"
-                (concat (concat (expand-file-name path (vendle:package-path package)) ":")
-                        (getenv "INFOPATH"))))
-      (vendle:package-info package)))))
+  (cl-letf ((info (vendle:package-info package)))
+    (cl-typecase info
+      (list
+       (cl-mapc
+        (lambda (path)
+          (setenv "INFOPATH"
+                  (concat (expand-file-name path (vendle:package-path package)) ":"
+                          (getenv "INFOPATH"))))
+        (vendle:package-info package)))
+      (string
+       (setenv "INFOPATH"
+               (concat (expand-file-name info (vendle:package-path package)) ":"
+                       (getenv "INFOPATH")))))))
 
 (provide 'vendle-option-info)
 
