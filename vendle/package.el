2bd7ed7b28e8eda9064953ed036dea4ed3d72af7
mytoh <mytoh@gmail.com>
2014-09-11 20:03:42 +0900

    extract local package source

--- vendle/package.el
+++ vendle/package.el
@@ -5,6 +5,7 @@
 
 (require 'vendle-source-github "vendle/source/github")
 (require 'vendle-source-git "vendle/source/git")
+(require 'vendle-source-local "vendle/source/local")
 
 ;;;; package
 
@@ -57,40 +58,6 @@
         ((vendle:source-git-p source)
          (vendle:make-package-git
           source info))))
-
-(cl-defun vendle:make-package-local (source info)
-  (cl-letf ((name (vendle:make-package-name-local source info))
-            (load-path (vendle:make-package-load-path-local source info)))
-    (vendle:package name
-                    :type 'local
-                    :name name
-                    :path source
-                    :load-path load-path
-                    :url ""
-                    :compile nil
-                    :build nil
-                    :info nil)))
-
-(cl-defun vendle:make-package-name-local (source info)
-  (if info
-      (thread-first info
-        (cl-getf :name)
-        (if name (file-name-nondirectory source)))
-    (file-name-nondirectory source)))
-
-(cl-defun vendle:make-package-load-path-local (source info)
-  (if info
-      (cl-letf ((path (cl-getf info :load-path)))
-        (if path
-            (expand-file-name path source)
-          source))
-    source))
-
-(cl-defun vendle:make-package-url-local (source info)
-  (if info
-      (if-let ((url (cl-getf info :url)))
-          url "")
-    ""))
 
 (provide 'vendle-package)
 
