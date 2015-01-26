69e08307b75d7fdaa6553e2277dfc7bdf7088f15
mytoh <mytoh@gmail.com>
2015-01-26 18:42:42 +0900

    * vendle/option/info.el: add internal function

--- vendle/option/info.el
+++ vendle/option/info.el
@@ -12,18 +12,20 @@
 
 (cl-defmethod vendle:option-info ((package vendle:package))
   (cl-letf ((info (vendle:package-info package)))
-    (cl-typecase info
-      (list
-       (seq-each
-        (lambda (path)
-          (vendle:option-info-set-infopath
-           (expand-file-name path (vendle:package-path package))))
-        info))
-      (string
-       (cl-letf ((path (expand-file-name info (vendle:package-path package))))
-         (vendle:message "add directory %s to INFOPATH" path)
-         (vendle:option-info-set-infopath
-          path))))))
+    (vendle:%option-info info package)))
+
+(cl-defmethod vendle:%option-info ((info list) (package vendle:package))
+  (seq-each
+   (lambda (path)
+     (vendle:option-info-set-infopath
+      (expand-file-name path (vendle:package-path package))))
+   info))
+
+(cl-defmethod vendle:%option-info ((info string) (package vendle:package))
+  (cl-letf ((path (expand-file-name info (vendle:package-path package))))
+    (vendle:message "add directory %s to INFOPATH" path)
+    (vendle:option-info-set-infopath
+     path)))
 
 (cl-defun vendle:option-info-set-infopath (path)
   (setenv "INFOPATH"
