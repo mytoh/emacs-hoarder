466332a3980bb2c157c9986334df79fedb56c3f4
mytoh <mytoh@gmail.com>
2015-02-11 00:43:45 +0900

    * vendle/option/build.el: use seq-each

--- vendle/option/build.el
+++ vendle/option/build.el
@@ -7,13 +7,14 @@
   (require 'cl-lib)
   (require 'eieio))
 
+(require 'seq)
 (require 'vendle-package "vendle/package")
 
 (cl-defmethod vendle:option-build ((package vendle:package))
   (when (vendle:package-build package)
     (cl-letf ((commands (vendle:package-build package))
               (path (file-name-as-directory (vendle:package-path package))))
-      (mapc
+      (seq-each
        (lambda (c)
          (cl-letf ((default-directory path))
            (shell-command c nil nil)))
