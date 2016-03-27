e405fc76815399e78751cf1793f3124c14a96380
mytoh <mytoh@gmail.com>
2016-03-27 22:56:48 +0900

    * .dir-locals.el: add nameless setting

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; .dir-locals -- .dir-locals -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(
+ (nameless-mode . (nameless-current-name . "hoarder"))
+ )
+
+
+;;; .dir-locals.el ends here
