fa7e7742e7c3193982c2b2a71248fc030233e070
mytoh <mytoh@gmail.com>
2017-09-26 14:09:40 +0900

    * lisp/hoarder/option/compile.el: force recompile files

--- lisp/hoarder/option/compile.el
+++ lisp/hoarder/option/compile.el
@@ -10,7 +10,7 @@
 
 (cl-defun hoarder:option-compile (package path)
   (if (glof:get package :compile)
-      (byte-recompile-directory path 0)))
+      (byte-recompile-directory path 0 'force)))
 
 (provide 'hoarder-option-compile)
 
